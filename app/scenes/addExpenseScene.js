8aaddc68a7eb371bb1bf89a4e6a76ae86cff6ecf
Yev Domin <yevhendomin@economist.com>
2023-04-08 13:13:55 +0100

    Price step started

--- app/scenes/addExpenseScene.js
+++ app/scenes/addExpenseScene.js
@@ -6,11 +6,7 @@ const { getCategoryMarkdownButtons } = require('../buttons');
 const categoryStep = new Composer();
 categoryStep.on('callback_query', async (ctx) => {
   try {
-    const callbackData = ctx.update.callback_query.data;
-    const buttonsIds = ctx.scene.session.buttonsIds;
-    const buttons = ctx.scene.session.buttons;
-    const clickedButton = buttons.find(button => button.callback_data === callbackData);
-    ctx.scene.session.category = parseInt(callbackData);
+    ctx.scene.session.category = parseInt(ctx.update.callback_query.data);
     await ctx.deleteMessage();
     await ctx.reply('Add Expense description: \n');
     return ctx.wizard.next();
@@ -26,13 +22,23 @@ const descriptionStep = new Composer();
 descriptionStep.on('message', async (ctx) => {
   try {
     ctx.scene.session.description = ctx.message.text;
-    ctx.reply(`Description: ${ctx.message.text}`);
+    ctx.reply(`Description: ${ctx.message.text} ${ctx.scene.session.category}. Please write the price:`);
+    return ctx.wizard.next();
   } catch (err) {
     ctx.reply('Error occured on Description step', err);
   }
 });
 
-const addExpenseScene = new Scenes.WizardScene('addExpenseScene', categoryStep, descriptionStep);
+const priceStep = new Composer();
+priceStep.on('message', async (ctx) => {
+  try {
+    ctx.reply('Price step. started!');
+  } catch (err) {
+    console.log('Error occured on adding price step\n', err);
+  }
+});
+
+const addExpenseScene = new Scenes.WizardScene('addExpenseScene', categoryStep, descriptionStep, priceStep);
 addExpenseScene.enter(async (ctx) => {
   const categories = await db.getAllCategoriesByTypeId(1);
   const buttons = await getCategoryMarkdownButtons();
